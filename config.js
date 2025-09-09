f0d8f3eca5d90b97c822f654fe8a487ec11ef9cb
Deyniik <d6694869@gmail.com>
2025-09-09 19:38:09 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// Инициализация Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyBk4nxZ1ldNQUCxsbgaeYTkm9stq0o9o2Q",
+    authDomain: "deygram-e4814.firebaseapp.com",
+    projectId: "deygram-e4814",
+    storageBucket: "deygram-e4814.appspot.com",
+    messagingSenderId: "591207

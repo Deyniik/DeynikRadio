9538ff80dfcb96aaa72fcbcdc67295484aedf941
Deyniik <d6694869@gmail.com>
2025-09-09 19:42:04 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,4 +4,11 @@ const firebaseConfig = {
     authDomain: "deygram-e4814.firebaseapp.com",
     projectId: "deygram-e4814",
     storageBucket: "deygram-e4814.appspot.com",
-    messagingSenderId: "591207
+    messagingSenderId: "591207353726",
+    appId: "1:591207353726:web:d20f5e648d3daf86fcb72f"
+};
+
+// Инициализация Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const database = firebase.database();
